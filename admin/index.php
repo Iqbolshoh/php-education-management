81095f25e0455f27722f1e9d6a47df4935c2c938
Iqbolshoh <iilhomjonov777@gmail.com>
2024-12-23 00:08:42 +0500

    commit

--- admin/index.php
+++ admin/index.php
@@ -10,6 +10,19 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 include './config.php';
 $query = new Database();
 
+$user = $query->select('users', '*')[0];
+$topics = $query->select('lessons', '*')[0];
+$results = $query->select('results', '*');
+
+$tests = count($query->select('test', '*'));
+$tru_falses = count($query->select('tru_false', '*'));
+$dropdowns = count($query->select('dropdown', '*'));
+$fill_in_the_blanks = count($query->select('fill_in_the_blank', '*'));
+$matchings = count($query->select('matching', '*'));
+$answers = count($query->select('answers', '*'));
+
+$question = $tests + $tru_falses + $dropdowns + $fill_in_the_blanks + $matchings + $answers;
+
 ?>
 
 <!DOCTYPE html>
@@ -46,14 +59,14 @@ $query = new Database();
 
               <div class="small-box bg-info">
                 <div class="inner">
-                  <h3>150</h3>
+                  <h3><?= $user['first_name'] ?></h3>
 
-                  <p>New Orders</p>
+                  <p><?= $user['email'] ?></p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="./user.php" class="small-box-footer">Edit User <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
 
@@ -61,28 +74,28 @@ $query = new Database();
 
               <div class="small-box bg-success">
                 <div class="inner">
-                  <h3>53<sup style="font-size: 20px">%</sup></h3>
+                  <h3><?= count($topics) . ' Topics' ?></sup></h3>
 
-                  <p>Bounce Rate</p>
+                  <p>Available in the system</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="./topics.php" class="small-box-footer">Add Topics <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
 
             <div class="col-lg-3 col-6">
               <div class="small-box bg-warning">
                 <div class="inner">
-                  <h3>44</h3>
+                  <h3><?= $question ?></h3>
 
-                  <p>User Registrations</p>
+                  <p>Total number of questions.</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-person-add"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="./test.php" class="small-box-footer">See questions <i class="fas fa-arrow-circle-right"></i></a>
               </div>
             </div>
 
@@ -90,14 +103,14 @@ $query = new Database();
 
               <div class="small-box bg-danger">
                 <div class="inner">
-                  <h3>65</h3>
+                  <h3><?= count($results) ?></h3>
 
-                  <p>Unique Visitors</p>
+                  <p>People passed the test.</p>
                 </div>
                 <div class="icon">
                   <i class="ion ion-pie-graph"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="./results.php" class="small-box-footer">View result <i class="fas fa-arrow-circle-right"></i></a>
               </div>
 
             </div>
